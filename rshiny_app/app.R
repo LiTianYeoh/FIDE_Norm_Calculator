30ac2624c47ce460f5de4d67415f834b984f2b1c
LiTianYeoh <yeohlitian@gmail.com>
2024-09-22 14:44:40 +0800

    minor update on reset button
    - reset button changed from NULL to ""
    - updated rsconnect

--- rshiny_app/app.R
+++ rshiny_app/app.R
@@ -804,7 +804,7 @@ server<- function(input, output){
       
       updateTextInput(
         inputId = name_id,
-        value = NULL
+        value = ""
       )
       
       updateNumericInput(
